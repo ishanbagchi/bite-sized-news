2c58ead98944bfe8c80caea6a0aea7fe6167b3f2
ishan-bagchi <ishan.bagchi@questionpro.com>
2025-03-03 02:25:09 +0530

    feat: add frontend boilerplate

---
+++ frontend/src/components/navbar.component.jsx
@@ -0,0 +1,32 @@
+import { Link } from 'react-router-dom'
+import logo from '../imgs/logo.png'
+
+const Navbar = () => {
+	return (
+		<nav className="navbar">
+			<Link to="/" className="flex items-center gap-2">
+				<img src={logo} className="w-10" alt="logo" />
+				<h1 className="font-bold text-2xl w-[210px] hidden md:block">
+					Bite-Sized News
+				</h1>
+			</Link>
+
+			<div className="absolute bg-white w-full left-0 top-full mt-0.5 border-b border-grey/50 py-4 px-[5vw] md:border-0 md:block md:relative md:inset-0 md:p-0 md:w-auto">
+				<input
+					type="search"
+					placeholder="Search"
+					className="w-full md:w-auto bg-grey/70 p-4 pl-6 pr-[12%] md:pr-6 rounded-full placeholder:text-dark-grey text-black md:pl-12"
+				/>
+				<i className="fi fi-bs-search absolute right-[10%] md:pointer-events-none md:left-5 top-1/2 -translate-y-1/2 text-xl text-dark-grey"></i>
+			</div>
+
+			<div className="flex items-center gap-3 md:gap-6 ml-auto">
+				<button className="md:hidden bg-grey w-12 h-12 rounded-full flex items-center justify-center">
+					<i className="fi fi-br-search text-xl"></i>
+				</button>
+			</div>
+		</nav>
+	)
+}
+
+export default Navbar
